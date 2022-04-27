7de6b663679447239c7ae5c00c8e5c7445bcb9b9
KBluePurple <42506968+KBluePurple@users.noreply.github.com>
2022-04-27 14:59:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function addComment() {
     var comment = document.getElementById('comment-text').value;
     var commentList = document.getElementById('comment-list');
     var newComment = document.createElement('div');
+    
+    if (name == "" || comment == "")
+        alert("빈칸을 채워주세요);
+    
     newComment.id = 'comment';
     newComment.innerHTML = `<div id="title"><b class="name"></b><a class="date"></a></div><br><p class="content"></p>`;
     commentList.appendChild(newComment);
