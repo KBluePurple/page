6466af56731a38d4b806a69679b4f14c93eadea8
KBluePurple <42506968+KBluePurple@users.noreply.github.com>
2022-04-27 14:55:37 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Hello World');
+
+function addComment() {
+    var name = document.getElementById('name-text').value;
+    var comment = document.getElementById('comment-text').value;
+    var commentList = document.getElementById('comment-list');
+    var newComment = document.createElement('div');
+    newComment.id = 'comment';
+    newComment.innerHTML = `<div id="title"><b class="name"></b><a class="date"></a></div><br><p class="content"></p>`;
+    commentList.appendChild(newComment);
+    newComment.getElementsByClassName('name')[0].innerHTML = name;
+    newComment.getElementsByClassName('date')[0].innerHTML = new Date().toLocaleString();
+    newComment.getElementsByClassName('content')[0].innerHTML = comment;
+}
